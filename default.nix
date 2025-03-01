8af7d371c1660c8054782f1169c80da336da7fd2
Stefan Frijters <sfrijters@gmail.com>
2025-03-01 02:07:14 +0100

    Fix overlay

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 final: prev: {
   qemu-espressif = final.callPackage ./packages/qemu-espressif { };
-  qemu-esp32 = final.callPackage ./packages/qemu-espressif { enableEsp32c3 = false; };
-  qemu-esp32c3 = final.callPackage ./packages/qemu-espressif { enableEsp32 = false; };
+  qemu-esp32 = final.callPackage ./packages/qemu-espressif { esp32c3Support = false; };
+  qemu-esp32c3 = final.callPackage ./packages/qemu-espressif { esp32Support = false; };
 }
