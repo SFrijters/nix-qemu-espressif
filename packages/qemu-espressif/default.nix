85e639362bc1443314024d2ba786adc6557fec6f
Stefan Frijters <sfrijters@gmail.com>
2026-08-08 12:30:26 +0200

    Fix nixfmt

--- packages/qemu-espressif/default.nix
+++ packages/qemu-espressif/default.nix
@@ -103,7 +103,9 @@ qemu'.overrideAttrs (
 
     # This patch in currently locked nixpkgs is for a newer version of qemu
     # than the base of this fork, so filter it out.
-    patches = builtins.filter (x: builtins.baseNameOf x != "fix-tls-tests-without-tasn1.patch") previousAttrs.patches;
+    patches = builtins.filter (
+      x: builtins.baseNameOf x != "fix-tls-tests-without-tasn1.patch"
+    ) previousAttrs.patches;
 
     buildInputs = [
       # dependencies declared in nixpkgs
